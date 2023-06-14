147dfd677756e5ebb59ec06712e42e540cb417ea
Silvina Gauto <silvigauto02@gmail.com>
2023-06-14 10:43:10 -0300

    inicio juego pantalla

--- main.py
+++ main.py
@@ -1,2 +1,30 @@
-print("probando github")
-print("probando segundo commit")
+import pygame, sys
+from configuraciones import *
+from pygame.locals import *
+
+
+#pantalla
+WIDTH = 1200
+HEIGHT = 600
+TAMAÑO_PANTALLA = (WIDTH,HEIGHT )
+FPS = 18
+RELOJ = pygame.time.Clock()
+PANTALLA  = pygame.display.set_mode(TAMAÑO_PANTALLA)
+
+
+fondo = pygame.image.load("Recursos\Fondos\campsite_by_dominique_van_velsen_deobxgw-fullview.jpg")
+fondo = pygame.transform.scale(fondo, TAMAÑO_PANTALLA)
+pygame.init()
+
+
+
+while True:
+    RELOJ.tick(FPS)
+    for evento in pygame.event.get():
+        if evento.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit(0)
+    
+
+    PANTALLA.blit(fondo, (0,0))
+    pygame.display.update()
